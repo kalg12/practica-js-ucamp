637fade05e5bd3bf79bf83baec13e47e11208196
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2025-02-27 20:32:25 -0600

    Agregando comentarios de lo que veremos

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 /* Para imprimir en pantalla usamos console.log() */
 console.log("Hola ucamp!");
+
+/* En la siguiente semana veremos:
+Uso de variables
+Concatenación
+Operaciones matemáticas
+Operadores
+Condicionales
+*/
